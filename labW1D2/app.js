cd009552627750d851048f0444b68cf280eb4b27
esh11 <52988928+esh11@users.noreply.github.com>
2021-03-24 08:51:05 -0400

    update for app

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -11,20 +11,23 @@ function load() {
 function nameString(obj) {
 	
 	let str = [];
-	if(obj=== {}){
-		return "object has no properties";
-	}else if (obj == undefined){
+	
+	 if (obj === undefined){
 		return "No object passed";
 	}else if (obj === null){
 		return "null argument"
 	}else {
 		for (let key in obj){
 			str.push(key);
-		}
+		}if(str.length==0){
+			return "object has no properties";
+		}else {
 		str.sort();
-		return str.join(".");
+		return str.join(".");}
 	}
 
 }
 
-console.log(nameString({}))
+console.log(nameString({}));
+console.log(nameString());
+console.log(nameString(null))
