0246c628f92b8a8f61666822e825f99625b6fb87
esh11 <52988928+esh11@users.noreply.github.com>
2021-03-23 23:02:22 -0400

    solution for day 2

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,28 @@
+"use strict";
 function load() {
 	let id = document.getElementById("result");
 	id.innerHTML = "Lab W1D2";
 }
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+
+
+// See tests.js for behavior
+function nameString(obj) {
+	let str = [];
+	if(obj == true){
+		return "object has no properties";
+	}else if (obj ==false){
+		return "No object passed";
+	}else if (obj === null){
+		return "null argument"
+	}else {
+		for (let key in obj){
+			str.push(key);
+		}
+		str.sort();
+		return str.join(".");
+	}
+
+}
+
