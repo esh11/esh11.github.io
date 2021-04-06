0386d26041d04187bd97d449a9f18766d6f35ff3
esh11 <52988928+esh11@users.noreply.github.com>
2021-04-06 16:53:50 -0400

    Create lab10.js

---
+++ assignments/lab10.js
@@ -0,0 +1,30 @@
+
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+}
+let user = {
+    name: 'John',
+    loginOk() {
+        alert(`${this.name} logged in`);
+    },
+    loginFail() {
+        alert(`${this.name} failed to log in`);
+    },
+};
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+askPassword(user.login.bind(user,true), user.login.bind(user,false)); 
+
+
+
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+    showList: function() { this.students.forEach(function(student) {
+                console.log(this.title + ": " + student);
+            }.bind(this));
+    }
+ };
+    group.showList();
